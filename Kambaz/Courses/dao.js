795bb3d2253ea0d1f971f47701d6d3c2b2937ac8
migchi430 <chica.m@northeastern.edu>
2025-12-05 15:36:39 -0500

    fixed minor issues

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,17 +1,18 @@
 import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
 
+const DEFAULT_IMAGE = "/images/NU_CMYK_Notched-N_motto_RW.png"
+
 export default function CoursesDao(_db) {
   function findAllCourses() {
-    // Only send name + description (and _id by default)
-    return model.find({}, { name: 1, description: 1 });
+    return model.find({});
   }
-
-  // NOTE: This is no longer used; the route now uses enrollmentsDao.findCoursesForUser()
-  // so we don't define findCoursesForEnrolledUser anymore.
 
   function createCourse(course) {
     const newCourse = { ...course, _id: uuidv4() };
+    if (!newCourse.image) {
+      newCourse.image = DEFAULT_IMAGE; 
+    }
     return model.create(newCourse);
   }
 
--- Kambaz/Courses/schema.js
+++ Kambaz/Courses/schema.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import moduleSchema from "../Modules/schema.js";
 
+const DEFAULT_IMAGE = "/images/NU_CMYK_Notched-N_motto_RW.png"
+
 const courseSchema = new mongoose.Schema(
   {
     _id: String,
@@ -8,6 +10,10 @@ const courseSchema = new mongoose.Schema(
     number: String,
     credits: Number,
     description: String,
+    image: {
+      type: String,
+      default: DEFAULT_IMAGE,
+    },
     modules: [moduleSchema],
   },
   { collection: "courses" }
