d4bb456be3715f9883b5949c6c03d3a5114320e1
migchi430 <chica.m@northeastern.edu>
2025-12-05 14:36:34 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import Hello from './Hello.js';
-import Lab5 from './Lab5/index.js';
 import cors from "cors";
 import db from "./Kambaz/Database/index.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
@@ -40,6 +39,5 @@ CourseRoutes(app, db);
 ModulesRoutes(app, db);
 AssignmentsRoutes(app, db);
 EnrollmentsRoutes(app, db);
-Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
