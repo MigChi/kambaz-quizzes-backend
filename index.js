5771035a9bd41a83fa4c49c5fe0866be01fe2f5c
migchi430 <chica.m@northeastern.edu>
2025-12-07 10:26:03 -0500

    fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import AssignmentsRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentsRoutes from './Kambaz/Enrollments/routes.js';
 import mongoose from 'mongoose';
 import QuizzesRoutes from "./Kambaz/Quizzes/routes.js";
-import QuizAttemptsRoutes from './Kambaz/quizzAttempts.js/routes.js';
+import QuizAttemptsRoutes from './Kambaz/QuizzAttempts/routes.js';
 
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
